9969ffcda01f35ec8b4bce0e5d14520643f13f78
Kris <k.a.veeken@student.rug.nl>
2019-11-21 20:29:10 +0100

    wrapping c++ into python is confusing

--- source/header.h
+++ source/header.h
@@ -4,9 +4,11 @@
 #include <vector>
 #include <tuple>
 
-// utils
+/***** utils *****/
+// smallest/largest value from a vector
 int indexSmallest(const std::vector<double> &vec);
 int indexLargest(const std::vector<double> &vec);
+// some vector operations
 std::vector<double> sclMult(const std::vector<double> &vec, double scl);
 std::vector<double> sclAdd(const std::vector<double> &vec, double scl);
 std::vector<double> vecAdd(const std::vector<double> &vec1, const std::vector<double> &vec2);
@@ -14,18 +16,29 @@ std::vector<double> vecSub(const std::vector<double> &vec1, const std::vector<do
 std::vector<std::vector<double> > vecCat(const std::vector<double> &vec1,
                                          const std::vector<double> &vec2,
                                          const std::vector<double> &vec3);
+double vecMean(const std::vector<double> &vec);
+// sort two vectors of length 3 by the elements in the err vector
 std::tuple<std::vector<std::vector<double> >, std::vector<double> >
   sort(const std::vector<std::vector<double> > &X, const std::vector<double> &err);
-double vecMean(const std::vector<double> &vec);
+// print a dot
 void dot();
-// Nelder-Mead
+
+/****** Nelder-Mead *****/
+// make an initial simplex from a guess
 std::vector<std::vector<double> > nelMeadInit(const std::vector<double> &guess);
 double objective(const std::vector<double> &t,
                  const std::vector<double> &y,
                  const std::vector<double> &Xi);
+// do the minimization
 std::tuple<double, std::vector<double> >  nelMead(const std::vector<std::vector<double> > &X,
                                                   const std::vector<double> &t,
                                                   const std::vector<double> &y,
                                                   int n);
+
+/****** regression *****/
+// make an initial guess
+std::vector<double> guess(std::vector<double> t, std::vector<double> y);
+// do the thing
+std::tuple<double,std::vector<double> > linReg(std::vector<double> t, std::vector<double> y);
 #endif
 
--- source/main.cpp
+++ source/main.cpp
@@ -5,31 +5,6 @@
 
 #include "header.h"
 
-std::vector<double> guess(std::vector<double> t, std::vector<double> y)
-{
-  double tmean = vecMean(t);
-  double ymean = vecMean(y);
-  double numsum = 0.0;
-  double densum = 0.0;
-  for(int i = 0; i < t.size();i++){
-    numsum += (t[i] - tmean) * (y[i] - ymean);
-    densum += (t[i] - tmean) * (t[i] - tmean);
-  }
-  std::vector<double> guess = {numsum / densum, ymean - numsum/densum * tmean};
-  return guess;
-}
-
-std::tuple<double,std::vector<double> > linReg(std::vector<double> t, std::vector<double> y)
-{
-  std::vector<double> guss = guess(t,y);
-  std::vector<std::vector<double> > X = nelMeadInit(guss);
-  double rmsd = 0;
-  std::vector<double> fit(2);
-  std::tie(rmsd,fit) = nelMead(X,t,y,0);
-  //std::cout << fit[0] << ' ' << fit[1];
-  return std::make_tuple(rmsd,fit);
-}
-
 int main()
 {
   std::vector<double> t = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,25,30,40,50};
@@ -41,3 +16,4 @@ int main()
             << "slope: " << fit[0] << " intercept: " << fit[1] << std::endl;
   return 0;
 }
+
